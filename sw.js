6d3e3f513d617faa301d13bbd933ad1b6d4f7f97
itemi123 <admir.halep@gmail.com>
2026-05-11 18:48:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,7 +21,6 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
